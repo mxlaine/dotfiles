a6f331b6fee4fd81e599a7d84efc43391ec7f4d2
Max Laine <maxmikaellaine@gmail.com>
2025-10-14 01:14:07 +0300

    init

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+fish_config theme choose "Dracula Official"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+zoxide init fish | source
+
+
+# pnpm
+set -gx PNPM_HOME "/home/max/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
---
+++ fish/functions/easyeffects.fish
@@ -0,0 +1,3 @@
+function easyeffects --wraps='flatpak run com.github.wwmm.easyeffects' --description 'alias easyeffects=flatpak run com.github.wwmm.easyeffects'
+    flatpak run com.github.wwmm.easyeffects $argv
+end
